b71df83ec85fe9e3e8fe18102875593706578df4
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-11 00:30:49 +0530

    Checked Search Show function of api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 // import {Link} from 'react-router-dom';
+import { useState } from "react";
 const Home=()=>{
+    const [searchStr,setSearchStr]=useState("");
+
+    const onSearchInputChange=(e)=>{
+        setSearchStr(e.target.value)
+    }
+    
+    const onSearch =  async (e) =>{
+       e.preventDefault();
+       const response = await fetch(
+         ` https://api.tvmaze.com/search/shows?q=${searchStr}`
+       );
+       const body =  await response.json();
+       console.log(body);
+    };
+
     return(
         <div>
+            <form onSubmit={onSearch}>
+                <input type="text" value={searchStr} onChange={onSearchInputChange}/>
+                <button type="submit">Search</button>
+            </form>
         </div>
     )
 }
