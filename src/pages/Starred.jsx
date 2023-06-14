06ae53fb67532294e54e57ac919bd34259389d26
PrathmeshModhe9 <lastry2001@gmail.com>
2023-06-14 13:00:46 +0530

    can not access useStarredShows from starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,8 +1,34 @@
-const Starred=()=>{
-    return(
-        <div>
-            <h1>Starred</h1>
-        </div>
-    )
-}
+import { useQuery } from '@tanstack/react-query';
+import { getShowById } from '../api/tvmaze';
+import  useStarredShows  from '../components/shows/useStarredShows.jsx';
+import ShowGrid from '../components/shows/ShowGrid';
+
+
+const Starred = () => {
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowById(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredShows?.length === 0) {
+    return <div>No shows are starred</div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  if (starredShowsError) {
+    return <div>Error occured: {starredShowsError.message}</div>;
+  }
+
+  return <div>Shows are loading</div>;
+};
+
 export default Starred;
