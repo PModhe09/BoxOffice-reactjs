57e9ae30d62fa35db3f759d1c982282bb91cdbb4
PrathmeshModhe9 <lastry2001@gmail.com>
2023-07-10 23:33:08 +0530

    commented starred page

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,32 +1,32 @@
-import { useQuery } from '@tanstack/react-query';
-import { getShowById } from '../api/tvmaze';
-import  {useStarredShows}  from '../components/UseStarredShows';
-import ShowGrid from '../components/shows/ShowGrid';
+// import { useQuery } from '@tanstack/react-query';
+// import { getShowById } from '../api/tvmaze';
+// import  {useStarredShows}  from '../components/UseStarredShows';
+// import ShowGrid from '../components/shows/ShowGrid';
 
 
 const Starred = () => {
-  const [starredShowsIds] = useStarredShows();
+  // const [starredShowsIds] = useStarredShows();
 
-  const { data: starredShows, error: starredShowsError } = useQuery({
-    queryKey: ['starred', starredShowsIds],
-    queryFn: () =>
-      getShowById(starredShowsIds).then(result =>
-        result.map(show => ({ show }))
-      ),
-    refetchOnWindowFocus: false,
-  });
+  // const { data: starredShows, error: starredShowsError } = useQuery({
+  //   queryKey: ['starred', starredShowsIds],
+  //   queryFn: () =>
+  //     getShowById(starredShowsIds).then(result =>
+  //       result.map(show => ({ show }))
+  //     ),
+  //   refetchOnWindowFocus: false,
+  // });
 
-  if (starredShows?.length === 0) {
-    return <div>No shows are starred</div>;
-  }
+  // if (starredShows?.length === 0) {
+  //   return <div>No shows are starred</div>;
+  // }
 
-  if (starredShows?.length > 0) {
-    return <ShowGrid shows={starredShows} />;
-  }
+  // if (starredShows?.length > 0) {
+  //   return <ShowGrid shows={starredShows} />;
+  // }
 
-  if (starredShowsError) {
-    return <div>Error occured: {starredShowsError.message}</div>;
-  }
+  // if (starredShowsError) {
+  //   return <div>Error occured: {starredShowsError.message}</div>;
+  // }
 
   return <div>Shows are loading</div>;
 };
